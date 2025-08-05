74ccb4074a3d011227524ead35b25887309a03fa
davidsungsukim <sheld2@naver.com>
2025-08-05 16:44:24 -0600

    1. add RingBuffer class as a utility.
    2. add tests for the RingBuffer.

---
+++ source/library/error_codes_lib.h
@@ -0,0 +1,43 @@
+/***************************************************************************************************
+ * @file           : error_codes_lib.h
+ * @brief          : Error codes for the library
+ * @details        : This file contains the error codes used in the library.
+ *                   Note that the application should define its own error codes in a separate file, and the codes should only be using the last 6 digits for compatibility.
+ * @author         : Sungsu Kim
+ * @date           : 2025-08-05
+ * @copyright      : Copyright (c) 2025 Sungsu Kim
+ ***************************************************************************************************/
+
+#pragma once
+
+/****************************************** Includes ***********************************************/ 
+#include <stdint.h>
+
+using ErrorCode = uint32_t;
+
+#define ERROR_LIB( x )        ( (uint32_t)( ErrorClass::eLIBRARY | x ) )
+#define ERROR_APP( x )        ( (uint32_t)( ErrorClass::eAPPLICATION | x ) )
+#define GET_ERROR_CLASS( x )  ( (uint32_t)x & 0xFF000000 )
+
+/******************************************** Types ************************************************/
+/**
+ * @brief Error class enumeration
+ */
+enum ErrorClass
+{
+   eLIBRARY     = 0x10000000,
+   eAPPLICATION = 0x20000000,
+};
+
+/**
+ * @brief Library error codes
+ */
+enum LibErrorCodes
+{
+   eOK = 0x00000000,
+   
+   eRING_BUFFER_INVALID_ARGUMENT  = ( eLIBRARY | 0x00000001 ),
+   eRING_BUFFER_EMPTY             = ( eLIBRARY | 0x00000002 ),
+   eRING_BUFFER_FULL              = ( eLIBRARY | 0x00000003 ),
+};
+
