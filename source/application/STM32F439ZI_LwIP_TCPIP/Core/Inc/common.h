5194b121d881ccdb032ba43da5ddc3de4e5eced3
davidsungsukim <sheld2@naver.com>
2025-08-05 21:00:34 -0600

    1. resolve a warning.

--- source/application/STM32F439ZI_LwIP_TCPIP/Core/Inc/common.h
+++ source/application/STM32F439ZI_LwIP_TCPIP/Core/Inc/common.h
@@ -9,7 +9,7 @@
 #define DEBUG_ETHERNET
 
 #if defined (USE_LOGGER)
-#define LOGGING( format, ... ) printf( "%08d: " format "\r\n", xTaskGetTickCount(), ##__VA_ARGS__ )
+#define LOGGING( format, ... ) printf( "%08ld: " format "\r\n", xTaskGetTickCount(), ##__VA_ARGS__ )
 #else 
 #define LOGGING( format, ... )
 #endif
