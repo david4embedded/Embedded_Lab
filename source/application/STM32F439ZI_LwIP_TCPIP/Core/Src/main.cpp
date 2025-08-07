8723ed7a68a6495ed66536130c8d0b3bc8c279ba
davidsungsukim <sheld2@naver.com>
2025-08-06 22:12:42 -0600

    1. change the timer to the timer6 since it's a basic timer that's available; timer1 is an advanced timer.

--- source/application/STM32F439ZI_LwIP_TCPIP/Core/Src/main.cpp
+++ source/application/STM32F439ZI_LwIP_TCPIP/Core/Src/main.cpp
@@ -224,14 +224,14 @@ static void MX_GPIO_Init(void)
 
 /**
  * @brief  Period elapsed callback in non blocking mode
- * @note   This function is called  when TIM1 interrupt took place, inside
+ * @note   This function is called  when TIM6 interrupt took place, inside
  *         HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment a global variable "uwTick" used as application time base.
  * @param  htim : TIM handle
  * @retval None
  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-   if (htim->Instance == TIM1) 
+   if (htim->Instance == TIM6) 
    {
       HAL_IncTick();
    }
