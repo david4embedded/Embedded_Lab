0bc8904524228bb9cf4786d64fc38d6a68df4069
davidsungsukim <sheld2@naver.com>
2025-08-11 16:26:04 -0600

    1. change two .c files to .cpp files.

---
+++ source/application/STM32F439ZI_LwIP_MQTT/Core/Src/freertos.cpp
@@ -0,0 +1,313 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include "lwip.h"
+#include "lwip/api.h"
+#include "MQTTClient.h"
+#include "MQTTInterface.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define BROKER_IP		"192.168.1.2"
+#define MQTT_PORT		1883
+#define MQTT_BUFSIZE	1024
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+extern struct netif gnetif; //extern gnetif
+
+osThreadId mqttClientSubTaskHandle;  //mqtt client task handle
+osThreadId mqttClientPubTaskHandle;  //mqtt client task handle
+
+Network net; //mqtt network
+MQTTClient mqttClient; //mqtt client
+
+uint8_t sndBuffer[MQTT_BUFSIZE]; //mqtt send buffer
+uint8_t rcvBuffer[MQTT_BUFSIZE]; //mqtt receive buffer
+uint8_t msgBuffer[MQTT_BUFSIZE]; //mqtt message buffer
+
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+void MqttClientSubTask(void const *argument); //mqtt client subscribe task function
+void MqttClientPubTask(void const *argument); //mqtt client publish task function
+int  MqttConnectBroker(void); 				//mqtt broker connect function
+void MqttMessageArrived(MessageData* msg); //mqtt message callback function
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const * argument);
+
+extern void MX_LWIP_Init(void);
+void MX_FREERTOS_Init(void);
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+extern "C" void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* Hook prototypes */
+extern "C" void vApplicationStackOverflowHook(xTaskHandle xTask, signed char *pcTaskName);
+
+/* USER CODE BEGIN 4 */
+extern "C" void vApplicationStackOverflowHook(xTaskHandle xTask, signed char *pcTaskName)
+{
+  /* Run time stack overflow checking is performed if
+   configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2. This hook function is
+   called if a stack overflow is detected. */
+  HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET); //turn on red led when detects stack overflow
+}
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 512);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const * argument)
+{
+  /* init code for LWIP */
+  MX_LWIP_Init();
+  /* USER CODE BEGIN StartDefaultTask */
+  osThreadDef(mqttClientSubTask, MqttClientSubTask, osPriorityNormal, 0, configMINIMAL_STACK_SIZE); //subscribe task
+  osThreadDef(mqttClientPubTask, MqttClientPubTask, osPriorityNormal, 0, configMINIMAL_STACK_SIZE); //publish task
+  mqttClientSubTaskHandle = osThreadCreate(osThread(mqttClientSubTask), NULL);
+
+  osDelay(1000);
+
+  mqttClientPubTaskHandle = osThreadCreate(osThread(mqttClientPubTask), NULL);
+
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+void MqttClientSubTask(void const *argument)
+{
+  printf( "start MQTT Subscribe Task\r\n");
+
+  while(1)
+  {
+    //waiting for valid ip address
+    if (gnetif.ip_addr.addr == 0 || gnetif.netmask.addr == 0 )//|| gnetif.gw.addr == 0) //system has no valid ip address
+    {
+      osDelay(1000);
+      continue;
+    }
+    else
+    {
+      printf("DHCP/Static IP O.K.\r\n");
+      break;
+    }
+  }
+
+  printf( "Run MQTT Subscribe Task\r\n");
+
+  while(1)
+  {
+    if(!mqttClient.isconnected)
+    {
+      //try to connect to the broker
+      MQTTDisconnect(&mqttClient);
+      MqttConnectBroker();
+      osDelay(1000);
+    }
+    else
+    {
+      MQTTYield(&mqttClient, 1000); //handle timer
+      osDelay(100);
+    }
+
+    //printf( "Run MQTT Subscribe Task\r\n");
+  }
+}
+
+void MqttClientPubTask(void const *argument)
+{
+  int count = 0;
+  uint8_t buff[64];
+
+  MQTTMessage message;
+
+  printf( "start MQTT Publish Task\r\n");
+
+  while(1)
+  {
+    if(mqttClient.isconnected)
+    {
+      memset(buff, 0, sizeof(buff));
+      snprintf((char*)buff, sizeof(buff), "%d", count++);
+      message.payload = (void*)buff;
+      message.payloadlen = strlen((char*)buff);
+
+      if(MQTTPublish(&mqttClient, "test", &message) != MQTT_SUCCESS)
+      {
+         printf( "MQTTPublish failed.\r\n" );
+        MQTTCloseSession(&mqttClient);
+        net_disconnect(&net);
+      }
+    }
+
+    osDelay(1000);
+    //printf( "Run MQTT Publish Task\r\n");
+  }
+}
+
+int MqttConnectBroker()
+{
+  int ret;
+
+  printf( "start MQTT Connect Broker\r\n");
+
+  NewNetwork(&net);
+  ret = ConnectNetwork(&net, BROKER_IP, MQTT_PORT);
+  if(ret != MQTT_SUCCESS)
+  {
+    printf("ConnectNetwork failed.\n");
+    net_disconnect(&net);
+    return -1;
+  }
+
+  printf( "MQTTPacket_connectData O.K\r\n");
+
+  MQTTClientInit(&mqttClient, &net, 1000, sndBuffer, sizeof(sndBuffer), rcvBuffer, sizeof(rcvBuffer));
+
+  MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+  data.willFlag = 0;
+  data.MQTTVersion = 3;
+  data.clientID.cstring = "STM32F4";
+  data.username.cstring = "STM32F4";
+  data.password.cstring = "";
+  data.keepAliveInterval = 60;
+  data.cleansession = 1;
+
+  ret = MQTTConnect(&mqttClient, &data);
+  if(ret != MQTT_SUCCESS)
+  {
+    printf("MQTTConnect failed.\n");
+    MQTTCloseSession(&mqttClient);
+    net_disconnect(&net);
+    return ret;
+  }
+
+  ret = MQTTSubscribe(&mqttClient, "test", QOS0, MqttMessageArrived);
+  if(ret != MQTT_SUCCESS)
+  {
+    printf("MQTTSubscribe failed.\n");
+    MQTTCloseSession(&mqttClient);
+    net_disconnect(&net);
+    return ret;
+  }
+
+  printf("MQTT_ConnectBroker O.K.\r\n");
+  return MQTT_SUCCESS;
+}
+
+void MqttMessageArrived(MessageData* msg)
+{
+  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin); //toggle pin when new message arrived
+
+  MQTTMessage* message = msg->message;
+  memset(msgBuffer, 0, sizeof(msgBuffer));
+  memcpy(msgBuffer, message->payload,message->payloadlen);
+
+  printf("MQTT MSG[%d]:%s\r\n", (int)message->payloadlen, msgBuffer);
+}
+
+/* USER CODE END Application */
--- source/application/STM32F439ZI_LwIP_MQTT/Core/Src/main.cpp
+++ source/application/STM32F439ZI_LwIP_MQTT/Core/Src/main.cpp
@@ -1,358 +1,218 @@
-/***************************************************************************************************
- * @file           : main.cpp
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
  * @brief          : Main program body
- * @details        : This file contains the main function and initializes the system.
- * @author         : Sungsu Kim
- * @date           : 2025-08-04
- * @copyright      : Copyright (c) 2025 Sungsu Kim
- ***************************************************************************************************/
-
-/****************************************** Includes ***********************************************/
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "usart.h"
-#include "common.h"
-#include "stm32f4xx_nucleo_144.h"
 #include "cmsis_os.h"
 #include "lwip.h"
-#include "lwip/tcp.h"
-#include "MQTTClient.h"
-#include "MQTTInterface.h"
-#include <string.h>
+#include "usart.h"
+#include "gpio.h"
 
-/******************************************** Consts ***********************************************/
-#define  BROKER_IP     "192.168.1.2"
-#define  MQTT_PORT	  1883
-#define  MQTT_BUFSIZE  1024
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-/**************************************** Global Variables *****************************************/
-extern struct netif gnetif;
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
 extern uint32_t MilliTimer;
+/* USER CODE END PV */
 
-/**************************************** Static Variables *****************************************/
-osThreadId  defaultTaskHandle;
-osThreadId  mqttClientSubTaskHandle;
-osThreadId  mqttClientPubTaskHandle;
-Network     net;
-MQTTClient  mqttClient;
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
 
-uint8_t sndBuffer[MQTT_BUFSIZE]; 
-uint8_t rcvBuffer[MQTT_BUFSIZE]; 
-uint8_t msgBuffer[MQTT_BUFSIZE];
+/* USER CODE END PFP */
 
-/**************************************** Local Functions ******************************************/
-static void    MX_GPIO_Init         ( );
-static void    SystemClock_Config   ( );
-static void    startDefaultTask     ( const void *argument );
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 
-static void    mqttClientSubTask    ( const void *argument );    
-static void    mqttClientPubTask    ( const void *argument ); 
-static int     mqttConnectBroker    ( ); 				  
-static void    mqttMessageArrived   ( MessageData* msg ); 
+/* USER CODE END 0 */
 
-/*************************************** Function Definitions **************************************/
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-   HAL_Init();
-   SystemClock_Config();
 
-   MX_GPIO_Init();
-   MX_USART3_UART_Init();
-   BSP_LED_Init( LED_RED );
-   BSP_LED_Init( LED_GREEN );
-   BSP_LED_Init( LED_BLUE );
+  /* USER CODE BEGIN 1 */
 
-   LOGGING( "Welcome to STM32F439ZI LwIP TCP/IP Application" );
+  /* USER CODE END 1 */
 
-   osThreadDef( defaultTask, startDefaultTask, osPriorityNormal, 0, 1024 );
-   defaultTaskHandle = osThreadCreate(osThread(defaultTask), nullptr );
+  /* MCU Configuration--------------------------------------------------------*/
 
-   osKernelStart();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-   while (1)
-   { }
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Call init function for freertos objects (in cmsis_os2.c) */
+  MX_FREERTOS_Init();
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief  Function implementing the defaultTask thread.
- * @param  argument: Not used
- * @retval None
- */
-static void startDefaultTask( const void *argument )
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
 {
-   MX_LWIP_Init();
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-   // osDelay(1000);      
-   // osThreadDef( mqttClientSub, mqttClientSubTask, osPriorityNormal, 0, 2048 );
-   // mqttClientSubTaskHandle = osThreadCreate( osThread( mqttClientSub ), nullptr );
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-   // osDelay(1000);      
-   // osThreadDef( mqttClientPub, mqttClientPubTask, osPriorityNormal, 0, 2048 );
-   // mqttClientPubTaskHandle = osThreadCreate( osThread( mqttClientPub ), nullptr );
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-   for(;;)
-   {    
-      osDelay(1000);
-      BSP_LED_Toggle( LED_BLUE );
-   }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-/**
- * @brief System Clock Configuration
- * @retval None
- */
-static void SystemClock_Config(void)
-{
-   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+/* USER CODE BEGIN 4 */
 
-   /** Configure the main internal regulator output voltage
-    */
-   __HAL_RCC_PWR_CLK_ENABLE();
-   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-   /** Initializes the RCC Oscillators according to the specified parameters
-    * in the RCC_OscInitTypeDef structure.
-    */
-   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-   RCC_OscInitStruct.PLL.PLLM = 8;
-   RCC_OscInitStruct.PLL.PLLN = 180;
-   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-   RCC_OscInitStruct.PLL.PLLQ = 4;
-   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-   {
-      Error_Handler();
-   }
-
-   /** Activate the Over-Drive mode
-    */
-   if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-   {
-      Error_Handler();
-   }
-
-   /** Initializes the CPU, AHB and APB buses clocks
-    */
-   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
-   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-   {
-      Error_Handler();
-   }
-}
+/* USER CODE END 4 */
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void)
-{
-   /* GPIO Ports Clock Enable */
-   __HAL_RCC_GPIOC_CLK_ENABLE();
-   __HAL_RCC_GPIOH_CLK_ENABLE();
-   __HAL_RCC_GPIOA_CLK_ENABLE();
-   __HAL_RCC_GPIOB_CLK_ENABLE();
-   __HAL_RCC_GPIOD_CLK_ENABLE();
-   __HAL_RCC_GPIOG_CLK_ENABLE();
-}
-
-/**
- * @brief  Period elapsed callback in non blocking mode
- * @note   This function is called  when TIM6 interrupt took place, inside
- *         HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment a global variable "uwTick" used as application time base.
- * @param  htim : TIM handle
- * @retval None
- */
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-   if ( htim->Instance == TIM6 ) 
-   {
-      HAL_IncTick();
-   }
+  /* USER CODE BEGIN Callback 0 */
 
-   if (htim->Instance == TIM6) 
-   {
-      MilliTimer++;
-   }
-}
-
-static void mqttClientSubTask( const void *argument )
-{
-   PARAM_NOT_USED( argument );
-
-   printf( "start MQTT Subscribe Task\r\n");
-
-  while(1)
-  {
-    //waiting for valid ip address
-    if (gnetif.ip_addr.addr == 0 || gnetif.netmask.addr == 0 )//|| gnetif.gw.addr == 0) //system has no valid ip address
-    {
-      osDelay(1000);
-      continue;
-    }
-    else
-    {
-      break;
-    }
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+  if (htim->Instance == TIM6) {
+    MilliTimer++;
   }
 
-  bool connectedOnce = false;
-
-  while(1)
-  {
-    if( !mqttClient.isconnected && !connectedOnce )
-    {
-      //try to connect to the broker
-      MQTTDisconnect(&mqttClient);
-      auto result = mqttConnectBroker();
-      if ( result == MQTT_SUCCESS )
-      {
-         connectedOnce = true;
-         printf( "mqtt connected once!\r\n" );
-      }
-      else
-      {
-         printf("mqttConnectBroker failed...abort\r\n");
-         abort();
-      }
-      osDelay(1000);
-    }
-    else
-    {
-      MQTTYield(&mqttClient, 1000); //handle timer
-      osDelay(100);
-    }
-
-    //printf( "Run MQTT Subscribe Task\r\n");
-  }
-}
-
-static void mqttClientPubTask( const void *argument )
-{
-   PARAM_NOT_USED( argument );
-
-  int count = 0;
-  uint8_t buff[64];
-
-  MQTTMessage message;
-
-  printf( "start MQTT Publish Task\r\n");
-
-  while(1)
-  {
-    if(mqttClient.isconnected)
-    {
-      memset(buff, 0, sizeof(buff));
-      snprintf((char*)buff, sizeof(buff), "%d", count++);
-      message.payload = (void*)buff;
-      message.payloadlen = strlen((char*)buff);
-
-      if(MQTTPublish(&mqttClient, "test", &message) != MQTT_SUCCESS)
-      {
-        printf( "MQTTPublish failed.\r\n" );
-        MQTTCloseSession(&mqttClient);
-        net_disconnect(&net);
-      }
-    }
-
-    osDelay(1000);
-    //printf( "Run MQTT Publish Task\r\n");
-  }
-}
-
-static int mqttConnectBroker()
-{
-  int ret;
-
-  printf( "start MQTT Connect Broker\r\n");
-
-  NewNetwork(&net);
-  ret = ConnectNetwork(&net, BROKER_IP, MQTT_PORT);
-  if(ret != MQTT_SUCCESS)
-  {
-    printf( "ConnectNetwork failed, ret=%d\r\n", ret );
-    net_disconnect(&net);
-    return -1;
-  }
-
-  MQTTClientInit(&mqttClient, &net, 1000, sndBuffer, sizeof(sndBuffer), rcvBuffer, sizeof(rcvBuffer));
-
-  MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-  data.willFlag = 0;
-  data.MQTTVersion = 3;
-  data.clientID.cstring = "STM32F4";
-  data.username.cstring = "STM32F4";
-  data.password.cstring = "";
-  data.keepAliveInterval = 60;
-  data.cleansession = 1;
-
-  ret = MQTTConnect(&mqttClient, &data);
-  if(ret != MQTT_SUCCESS)
-  {
-    printf("MQTTConnect failed.\r\n");
-    MQTTCloseSession(&mqttClient);
-    net_disconnect(&net);
-    return ret;
-  }
-
-  ret = MQTTSubscribe(&mqttClient, "test", QOS0, mqttMessageArrived);
-  if(ret != MQTT_SUCCESS)
-  {
-    printf("MQTTSubscribe failed.\r\n");
-    MQTTCloseSession(&mqttClient);
-    net_disconnect(&net);
-    return ret;
-  }
-
-  printf( "mqttConnectBroker... done\r\n " );
-  return MQTT_SUCCESS;
-}
-
-static void mqttMessageArrived(MessageData* msg)
-{
-  //HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin); //toggle pin when new message arrived
-
-  MQTTMessage* message = msg->message;
-  memset(msgBuffer, 0, sizeof(msgBuffer));
-  memcpy(msgBuffer, message->payload,message->payloadlen);
-
-  printf("MQTT MSG[%d]:%s\r\n", (int)message->payloadlen, msgBuffer);
+  /* USER CODE END Callback 1 */
 }
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-   __disable_irq();
-   while (1)
-   { }
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
