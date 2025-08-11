5b89af30938dd73cf616eeda2941f9142cca3454
davidsungsukim <sheld2@naver.com>
2025-08-11 14:40:38 -0600

    1. add the declaration of MQTTCloseSession() or it doesn't compile.

--- source/application/STM32F439ZI_LwIP_MQTT/Middlewares/Third_Party/paho.mqtt/MQTTClient.h
+++ source/application/STM32F439ZI_LwIP_MQTT/Middlewares/Third_Party/paho.mqtt/MQTTClient.h
@@ -219,6 +219,8 @@ DLLExport int MQTTYield(MQTTClient* client, int time);
  */
 DLLExport int MQTTIsConnected(MQTTClient* client);
 
+DLLExport void MQTTCloseSession(MQTTClient* c);
+
 #if defined(MQTT_TASK)
 /** MQTT start background thread for a client.  After this, MQTTYield should not be called.
 *  @param client - the client object to use
