1e7012b20cec30617450166acb7a0e51d7a3bb8a
davidsungsukim <sheld2@naver.com>
2025-08-14 15:24:31 -0600

    1. set up the unit test environment for the cli module.

---
+++ unit_test/cli/cli_tests.cpp
@@ -0,0 +1,35 @@
+/************************************************************************************************************
+ *
+ * @file cli_test.cpp
+ * @brief Unit tests for the CLI class
+ *
+ * @author Sungsu Kim
+ * @copyright 2025 Sungsu Kim
+ * @date 2025-08-14
+ * @version 1.0
+ *
+ ************************************************************************************************************/
+
+ /************************************************** Includes ************************************************/
+#include "ring_buffer.h"
+#include <gtest/gtest.h>
+
+/************************************************** Test Fixture ********************************************/
+class CliTest : public ::testing::Test
+{
+protected:
+   void SetUp() override
+   { }
+   void TearDown() override
+   { }
+public:
+};
+
+/************************************************** Tests ***************************************************/
+/**
+ * @brief Test for RingBuffer push and pop operations
+ */
+TEST_F(CliTest, test_all )
+{
+   
+}
