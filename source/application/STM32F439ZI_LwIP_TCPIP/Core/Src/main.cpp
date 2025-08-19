46cdbf7de39a3175b575fd265ff452198d3b9852
davidsungsukim <sheld2@naver.com>
2025-08-18 21:53:39 -0600

    1. UART Tx is now done on the interrupt mode. Also, when a Rx frame is received, the interrupt routine is invoked so the application can make use of. For this, some custom changes are made in usart.c and stm32f4xx_it.c.

--- source/application/STM32F439ZI_LwIP_TCPIP/Core/Src/main.cpp
+++ source/application/STM32F439ZI_LwIP_TCPIP/Core/Src/main.cpp
@@ -101,7 +101,7 @@ static void SystemClock_Config(void)
  */
 extern "C" int _write(int file, char *ptr, int len)
 {
-   HAL_UART_Transmit(&huart3, (uint8_t*)ptr, len, HAL_MAX_DELAY);
+   HAL_UART_Transmit_IT(&huart3, (uint8_t*)ptr, len); 
 }
 
 /**
