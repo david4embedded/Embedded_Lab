b6aca5320342ca7df4f921c750f0a71006578e0a
davidsungsukim <sheld2@naver.com>
2025-08-20 14:48:13 -0600

    1. cleanup

--- unit_test/cli/cli_tests.cpp
+++ unit_test/cli/cli_tests.cpp
@@ -60,42 +60,37 @@ TEST_F( CliTest, test_tokenize_works_properly )
 {
    char str[128] = { 0 };
    snprintf( str, sizeof( str ), "%s", "command arg1 arg2 arg3 arg4\r" );
-    
+
    auto& cli = lib::CLI::getInstance();
 
    char* argv[10];
-   auto argc = cli.tokenize( str, argv, 10 );
+   int argc = cli.tokenize( str, argv, 10 );
 
    EXPECT_EQ( argc, 5 );
-   EXPECT_EQ( strcmp(argv[0], "command"), 0 );
-   EXPECT_EQ( strcmp(argv[1], "arg1"), 0 );
-   EXPECT_EQ( strcmp(argv[2], "arg2"), 0 );
-   EXPECT_EQ( strcmp(argv[3], "arg3"), 0 );
+   EXPECT_STREQ( argv[0], "command" );
+   EXPECT_STREQ( argv[1], "arg1" );
+   EXPECT_STREQ( argv[2], "arg2" );
+   EXPECT_STREQ( argv[3], "arg3" );
 }
 
 TEST_F( CliTest, test_putting_characters_and_getting_new_lines_work_properly )
 {
    auto& cli = lib::CLI::getInstance();
 
-   char str[128] = { 0 };
-   snprintf( str, sizeof( str ), "%s", "test arg1\rtest arg2\r" );
+   char str[] = "test arg1\rtest arg2\r";
 
    EXPECT_CALL( m_semaphoreMock, putISR() ).Times( 2 );
 
-   auto length = strlen( str );
-   auto* ptr = str;
-   while ( length-- )
+   for ( const char* ptr = str; *ptr != '\0'; ++ptr )
    {
-      cli.putCharIntoBuffer( *ptr++ );
+      cli.putCharIntoBuffer( *ptr );
    }
 
-   char bufferNewCommand[128];
+   char bufferNewCommand[128] = { 0 };
 
    EXPECT_CALL( m_semaphoreMock, get( ::testing::_ ) ).Times( 1 );
-   memset( bufferNewCommand, 0, sizeof( bufferNewCommand ) );
    cli.getNewCommandLine( bufferNewCommand, sizeof( bufferNewCommand ) );
 
-   
    EXPECT_CALL( m_semaphoreMock, get( ::testing::_ ) ).Times( 1 );
    memset( bufferNewCommand, 0, sizeof( bufferNewCommand ) );
    cli.getNewCommandLine( bufferNewCommand, sizeof( bufferNewCommand ) );
