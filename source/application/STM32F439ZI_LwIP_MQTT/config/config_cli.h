2a34daed143f4f7850b44fa031f7f8a5dedbd253
davidsungsukim <sheld2@naver.com>
2025-08-20 15:23:23 -0600

    1. add files for new modules made in the TCP/IP project to the MQTT project as well.

---
+++ source/application/STM32F439ZI_LwIP_MQTT/config/config_cli.h
@@ -0,0 +1,24 @@
+/************************************************************************************************************
+ * 
+ * @file config_cli.h
+ * @brief Configuration of CLI module and user-defined commands
+ *  
+ * @author Sungsu Kim
+ * @copyright 2025 Sungsu Kim
+ * @date 2025-08-20
+ * @version 1.0
+ * 
+ ************************************************************************************************************/
+
+#pragma once
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+/******************************************* Function Declarations ******************************************/    
+void CLI_putCharIntoBuffer( char c );
+
+#if defined (__cplusplus)
+}
+#endif
